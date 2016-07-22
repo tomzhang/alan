ee579ba15d5ac4bc0a4bb387508d1c984236c7de
wanghongfei <brucewhf@gmail.com>
2016-07-22 11:15:37 +0800

    搭建eureka项目

---
+++ alan-registry/src/main/java/cn/com/sina/alan/config/AlanConfigServer.java
@@ -0,0 +1,16 @@
+package cn.com.sina.alan.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by whf on 7/22/16.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class AlanConfigServer {
+    public static void main(String[] args) {
+        SpringApplication.run(AlanConfigServer.class);
+    }
+}
