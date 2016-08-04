551e46aca50bb0cf681f6e05ec0033ad4b6ca4ae
sihan2 <sihan2@staff.sina.com.cn>
2016-08-04 10:36:44 +0800

    调整包路径，修改pom文件model部分

--- alan-config/src/main/java/cn/com/sina/alan/ConfigServerApplication.java
+++ alan-config/src/main/java/cn/com/sina/alan/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package cn.com.sina.alan.config;
+package cn.com.sina.alan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
