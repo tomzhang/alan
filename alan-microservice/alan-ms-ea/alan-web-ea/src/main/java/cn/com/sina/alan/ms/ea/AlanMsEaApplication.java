518fb3a160443bdeda53cc846ed76f09f9ba932f
wanghongfei <brucewhf@gmail.com>
2016-08-09 23:20:16 +0800

    完成ea模块

---
+++ alan-microservice/alan-ms-ea/alan-web-ea/src/main/java/cn/com/sina/alan/ms/ea/AlanMsEaApplication.java
@@ -0,0 +1,15 @@
+package cn.com.sina.alan.ms.ea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by whf on 8/9/16.
+ */
+@SpringBootApplication
+public class AlanMsEaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlanMsEaApplication.class, args);
+    }
+}
