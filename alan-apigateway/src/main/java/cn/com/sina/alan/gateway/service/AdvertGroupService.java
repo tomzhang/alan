4c8b53120b04b8317e521b298a1fde1931dd9d01
wanghongfei <brucewhf@gmail.com>
2016-08-19 18:19:59 +0800

    hystrix fallback用法

--- alan-apigateway/src/main/java/cn/com/sina/alan/gateway/service/AdvertGroupService.java
+++ alan-apigateway/src/main/java/cn/com/sina/alan/gateway/service/AdvertGroupService.java
@@ -17,7 +17,7 @@ public class AdvertGroupService {
     @Autowired
     private AdvertGroupRemoteService adGroupRemoteService;
 
-    @HystrixCommand(//fallbackMethod = "fallback",
+    @HystrixCommand(fallbackMethod = "fallback",
             ignoreExceptions = AlanException.class,
             commandProperties = {
         @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
@@ -26,9 +26,11 @@ public class AdvertGroupService {
         return adGroupRemoteService.findByGroupId(groupId);
     }
 
-    private AdvertGroupVO fallback(Integer id) {
+    private AdvertGroupVO fallback(Integer id, Throwable throwable) {
         AdvertGroupVO vo = new AdvertGroupVO();
-        vo.setGroupName("fallback");
+        vo.setGroupName(throwable.getMessage());
+
+        //throwable.printStackTrace();
 
         return vo;
     }
