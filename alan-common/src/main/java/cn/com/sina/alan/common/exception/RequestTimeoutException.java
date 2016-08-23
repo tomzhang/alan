e9a729a6a98ce8e627b14c3636713766242a0809
wanghongfei <brucewhf@gmail.com>
2016-08-23 19:04:10 +0800

    去除无用Exception类

--- alan-common/src/main/java/cn/com/sina/alan/common/exception/RequestTimeoutException.java
+++
@@ -1,9 +0,0 @@
-package cn.com.sina.alan.common.exception;
-
-/**
- * Created by whf on 8/5/16.
- */
-public class RequestTimeoutException extends AlanException {
-    public RequestTimeoutException() {
-    }
-}
