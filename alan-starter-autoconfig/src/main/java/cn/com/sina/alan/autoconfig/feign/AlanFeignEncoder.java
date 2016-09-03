a2d852abb0aa0752daac231eeb8c1bc4336cdb9c
wanghongfei <brucewhf@gmail.com>
2016-09-03 23:00:16 +0800

    支持文件上传

--- alan-starter-autoconfig/src/main/java/cn/com/sina/alan/autoconfig/feign/AlanFeignEncoder.java
+++ alan-starter-autoconfig/src/main/java/cn/com/sina/alan/autoconfig/feign/AlanFeignEncoder.java
@@ -2,14 +2,19 @@ package cn.com.sina.alan.autoconfig.feign;
 
 import feign.RequestTemplate;
 import feign.codec.EncodeException;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.CharUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.cloud.netflix.feign.support.SpringEncoder;
 import org.springframework.util.ReflectionUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.lang.reflect.Type;
+import java.nio.charset.Charset;
 
 /**
  * Created by whf on 8/22/16.
@@ -26,7 +31,40 @@ public class AlanFeignEncoder extends SpringEncoder {
     public void encode(Object requestBody, Type bodyType, RequestTemplate request) throws EncodeException {
         log.debug("编码{}对象,类型为{}", requestBody, bodyType);
 
+        // 如果是MultipartFile类型的参数
+        // 说明该参数是要上传的文件
+        if (requestBody instanceof MultipartFile) {
+            processMultipartFile((MultipartFile) requestBody, request);
 
+        } else {
+            // 是POJO对象
+            processPOJO(requestBody, request);
+
+        }
+
+
+    }
+
+    /**
+     * 将byte数组取出, 通过Base64编码后放入请求体中
+     * @param file
+     * @param request
+     */
+    private void processMultipartFile(MultipartFile file, RequestTemplate request) {
+        try {
+            byte[] buf = file.getBytes();
+            byte[] base64Buf = Base64.encodeBase64(buf);
+
+            request.header("Content-Type", "text/plain");
+            request.body(base64Buf, Charset.forName("UTF-8"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new EncodeException(e.getMessage());
+        }
+    }
+
+    private void processPOJO(Object requestBody, RequestTemplate request) {
         StringBuilder sb = new StringBuilder();
         ReflectionUtils.doWithFields(requestBody.getClass(), field -> {
             field.setAccessible(true);
