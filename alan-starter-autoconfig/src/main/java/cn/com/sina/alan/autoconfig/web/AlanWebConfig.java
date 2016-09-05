147d94d958031a8ab6a5f5196ba0f99edde5f086
wanghongfei <brucewhf@gmail.com>
2016-09-05 19:44:18 +0800

    添加Apache HttpClient配置

--- alan-starter-autoconfig/src/main/java/cn/com/sina/alan/autoconfig/web/AlanWebConfig.java
+++ alan-starter-autoconfig/src/main/java/cn/com/sina/alan/autoconfig/web/AlanWebConfig.java
@@ -1,7 +1,14 @@
 package cn.com.sina.alan.autoconfig.web;
 
+import feign.Feign;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.ConnectionConfig;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +24,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class AlanWebConfig extends WebMvcConfigurerAdapter {
     public static final Logger log = LoggerFactory.getLogger(AlanWebConfig.class);
 
+    public static int HTTP_SO_TIMEOUT = 5;
+    public static int HTTP_CONN_TIMEOUT = 5;
+
 
     /**
      * 只有当不是ApiGateway时才有效
@@ -28,6 +38,25 @@ public class AlanWebConfig extends WebMvcConfigurerAdapter {
     public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
         log.info("AlanJsonHttpMessageConverter已启用");
         return new AlanJsonHttpMessageConverter();
+    }
+
+    @Bean
+    @ConditionalOnClass(Feign.class)
+    public HttpClient httpClient() {
+        log.info("AlanHttpClient已启用. so_timeout = {}, conn_timeout = {}", HTTP_SO_TIMEOUT, HTTP_CONN_TIMEOUT);
+
+        return HttpClients.custom()
+                .setDefaultRequestConfig(httpRequestConfig())
+                .build();
+    }
+
+    public RequestConfig httpRequestConfig() {
+
+
+        return RequestConfig.custom()
+                .setSocketTimeout(HTTP_SO_TIMEOUT)
+                .setConnectTimeout(HTTP_CONN_TIMEOUT)
+                .build();
     }
 
 }
