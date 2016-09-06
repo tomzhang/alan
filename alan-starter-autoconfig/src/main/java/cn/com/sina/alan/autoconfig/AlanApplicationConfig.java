2aa5585c79e823b899cf201386334938e5b4dce6
wanghongfei <brucewhf@gmail.com>
2016-09-06 10:46:21 +0800

    添加公共配置

--- alan-starter-autoconfig/src/main/java/cn/com/sina/alan/autoconfig/AlanApplicationConfig.java
+++ alan-starter-autoconfig/src/main/java/cn/com/sina/alan/autoconfig/AlanApplicationConfig.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 /**
  * 免去添加@EnableXXX注解
@@ -16,6 +18,10 @@ import org.springframework.context.annotation.Configuration;
 //@EnableFeignClients(basePackages = "cn.com.sina.alan", defaultConfiguration = AlanFeignConfig.class)
 @EnableFeignClients(basePackages = "cn.com.sina.alan")
 @EnableHystrix
+
+@PropertySources({
+    @PropertySource("classpath:alan.properties")
+})
 
 // 只有当alan.auto.enable=true或没有时才应用该配置类
 @ConditionalOnProperty(prefix = "alan.auto", name = "enable", matchIfMissing = true, havingValue = "true")
