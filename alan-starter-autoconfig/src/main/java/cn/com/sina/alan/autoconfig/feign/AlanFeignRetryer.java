fd205f93ef549bea3407b7d0e5b9ee8f4789a63a
wanghongfei <brucewhf@gmail.com>
2016-11-08 11:16:48 +0800

    Feign重试器日志增强

--- alan-starter-autoconfig/src/main/java/cn/com/sina/alan/autoconfig/feign/AlanFeignRetryer.java
+++ alan-starter-autoconfig/src/main/java/cn/com/sina/alan/autoconfig/feign/AlanFeignRetryer.java
@@ -6,15 +6,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * Feign的重试器
  * Created by wanghongfei(hongfei7@staff.sina.com.cn) on 9/5/16.
  */
 public class AlanFeignRetryer implements Retryer {
     private static final Logger log = LoggerFactory.getLogger(AlanFeignRetryer.class);
 
+    /**
+     * 如果将异常抛出, 则Feign就会取消重试
+     * @param e
+     */
     @Override
     public void continueOrPropagate(RetryableException e) {
-        log.debug("传递异常{}", e.getMessage());
+        log.debug("重试异常{}", e.getMessage());
+        log.warn("连接失败或超时, 不再重试. 连接异常信息: {}", e.getMessage());
 
+        // 抛出异常不再重试
         throw e;
     }
 
