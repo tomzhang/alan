71bc7fac738498104acf05ac1a90a1bdd2b10dce
wanghongfei <brucewhf@gmail.com>
2016-11-16 15:42:40 +0800

    feign encoder支持将POJO编码到GET请求的URL中

--- alan-starter-autoconfig/src/main/java/cn/com/sina/alan/autoconfig/feign/AlanFeignEncoder.java
+++ alan-starter-autoconfig/src/main/java/cn/com/sina/alan/autoconfig/feign/AlanFeignEncoder.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.cloud.netflix.feign.support.SpringEncoder;
+import org.springframework.http.HttpMethod;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,6 +17,7 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.charset.Charset;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -72,28 +74,71 @@ public class AlanFeignEncoder extends SpringEncoder {
     }
 
     private void processPOJO(Object requestBody, RequestTemplate request) {
-        StringBuilder sb = new StringBuilder();
-        ReflectionUtils.doWithFields(requestBody.getClass(), field -> {
+        // 将POJO的属性转换成键值对
+        Map<String, String> parameterMap = parseParameterMap(requestBody);
+
+        // 判断是否为GET请求
+        if (isGetMethod(request)) {
+            // k-v放到URL中
+            parameterMap.entrySet().stream().forEach( entry -> {
+                request.query(false, entry.getKey(), entry.getValue());
+            } );
+
+            log.debug("将参数 {} 添加到URL中", parameterMap);
+
+        } else {
+            // 非GET请求, 参数扔到请求体中
+            String queryString = buildQueryString(parameterMap);
+            request.header("Content-Type", "application/x-www-form-urlencoded");
+            request.body(queryString);
+
+            log.debug("将参数 {} 添加到请求体中", queryString);
+        }
+
+    }
+
+    private boolean isGetMethod(RequestTemplate request) {
+        return request.method().equals(HttpMethod.GET.name());
+    }
+
+    /**
+     * 将POJO转换成key-value参数对
+     * @param body
+     * @return
+     */
+    private Map<String, String> parseParameterMap(Object body) {
+        Map<String, String> parameterMap = new HashMap<>();
+
+        ReflectionUtils.doWithFields(body.getClass(), field -> {
             field.setAccessible(true);
 
-            Object value = field.get(requestBody);
+            Object value = field.get(body);
             if (null != value) {
-                String name = field.getName();
-                log.debug("添加参数{}={}", name, value);
-                //request.query(name, value.toString());
 
-                sb.append(name);
-                sb.append("=");
-                sb.append(value.toString());
-                sb.append("&");
+                String name = field.getName();
+                parameterMap.put(name, value.toString());
+
+                log.debug("从POJO中解析出参数: {}={}", name, value.toString());
             }
         });
 
-        String postBody = sb.toString();
-        log.debug("POST请求体:{}", postBody);
+        return parameterMap;
+    }
 
-        request.header("Content-Type", "application/x-www-form-urlencoded");
-        request.body(postBody);
+    /**
+     * 将参数对转换为query string, 如 a=b&c=d&e=f
+     * @param parameterMap
+     * @return
+     */
+    private String buildQueryString(Map<String, String> parameterMap) {
+        StringBuilder sb = new StringBuilder(parameterMap.size() * 8);
+
+        for (Map.Entry<String, String> entry : parameterMap.entrySet()) {
+            sb.append(entry.getKey());
+            sb.append(entry.getValue());
+        }
+
+        return sb.toString();
     }
 
 }
